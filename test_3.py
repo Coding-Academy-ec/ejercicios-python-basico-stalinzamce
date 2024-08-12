6f8065030b144e8aa5d50e5893ebf6f499174f17
stalinzamce <stalinzamce@gmail.com>
2024-08-12 09:56:52 -0500

    Actualizado

--- test_3.py
+++ test_3.py
@@ -3,9 +3,9 @@ from io import StringIO
 import sys
 
 def test_imprimir_datos_personales(capsys):
-    nombre = "Tu nombre"
-    edad = 30
+    nombre = "Gilbert"
+    edad = 34
     estatura = 1.75
     imprimir_datos_personales(nombre, edad, estatura)
     captured = capsys.readouterr()
-    assert captured.out == "Nombre: Tu nombre\nEdad: 30\nEstatura: 1.75\n"
+    assert captured.out == "Nombre: Gilbert\nEdad: 34\nEstatura: 1.75\n"
